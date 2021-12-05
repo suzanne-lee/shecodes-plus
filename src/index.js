db030e7889939733677bb06eaeaf93cd0fd06da3
Suzanne Lee <suzannebhlee@gmail.com>
2021-12-05 17:44:45 -0500

    remove garbage comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 let now = new Date();
-// let apiKey = "69783318a51bc25d3112e1ead9cef0cd";
 let apiKey = "69783318a51bc25d3112e1ead9cef0cd";
 
-// altTextObject[iconId];
 const altTextObject = {
   "01d": "Yellow Sun",
   "01n": "Purple Crescent Moon surrounded by Yellow Stars",
@@ -25,26 +23,27 @@ const altTextObject = {
   "50n": "Purple Crescent Moon with White Squiggly Mist Lines",
 };
 
-// day-cloudy-sky-background: between sunrise and sunset, cloudy
-// day-clear-sky-background: between sunrise and sunset, clear sky
-// morning-evening-sky-background: sunrise/sunset
-// night-sky-background: night
-
 function changeBackground(imageName) {
   document.querySelector(
     "body"
   ).style.backgroundImage = `url(backgrounds/${imageName}.png)`;
-  // element.style.backgroundColor = color;
   document.body.style.backgroundHeight = "90%";
 }
 
 function changeTextColor(hexCode) {
+  // Date/time, Change location input/buttons
   document.getElementById("upper-container").style.color = hexCode;
 
   document.querySelector("input").style.borderColor = hexCode;
   document.querySelector("input").style.color = hexCode;
 
-  // location
+  if (hexCode == "#fff") {
+    document.querySelector("#search-city").classList.add("night");
+  } else {
+    document.querySelector("#search-city").classList.remove("night");
+  }
+
+  // Current Location
   document.getElementById("location").style.color = hexCode;
 
   // Next 5 days
@@ -54,12 +53,6 @@ function changeTextColor(hexCode) {
   document.querySelector("hr").style.color = hexCode;
   document.querySelector("hr").style.borderTopColor = hexCode;
   document.getElementById("footer").style.color = hexCode;
-
-  if (hexCode == "#fff") {
-    document.querySelector("#search-city").classList.add("night");
-  } else {
-    document.querySelector("#search-city").classList.remove("night");
-  }
 }
 
 function displayCurrentDate(date) {
@@ -154,8 +147,6 @@ function displayCurrentWeather(response) {
   let weather = response.data.weather[0].main;
   currWeatherElement.innerHTML = weather;
 
-  // console.log(response.data);
-
   // Feels like Temperature
   let feelsLikeTempElement = document.getElementById("card1-feels-like-temp");
   let feelsLikeTemp = Math.round(response.data.main.feels_like);
@@ -188,25 +179,14 @@ function displayCurrentWeather(response) {
   sunrise = new Date(sunrise);
   sunset = new Date(sunset);
 
-  // console.log("Sunrise: " + sunrise.getTime());
-  //console.log(sunset);
-
   sunriseFormattedTime = formatTime(sunrise);
   sunsetFormattedTime = formatTime(sunset);
 
   sunriseElement.innerHTML = sunriseFormattedTime;
   sunsetElement.innerHTML = sunsetFormattedTime;
 
-  // console.log(sunrise);
-
   let iconId = response.data.weather[0].icon;
-  //let imgElement = document.getElementById("current-weather-icon");
-  // imgElement.src = `weather_icons/${iconId}.png`;
-
   displayIcon("current-weather-icon", iconId);
-
-  // setAltText(imgId, iconId)
-  // setAltText("current-weather-icon", iconId);
 
   // CHANGE BACKGROUND HERE
   let currentDate = new Date();
@@ -215,10 +195,6 @@ function displayCurrentWeather(response) {
   let sunsetTime = sunset.getTime();
 
   let halfHour = (3600 / 2) * 1000;
-
-  //console.log("Current Time: " + currentTime);
-  //console.log("Sunrise Time: " + sunriseTime);
-  //console.log("Sunset Time: " + sunsetTime);
 
   if (
     currentTime > sunriseTime + halfHour &&
@@ -236,7 +212,6 @@ function displayCurrentWeather(response) {
   ) {
     // use night sky image
     changeBackground("night");
-    // changeBackground("sunset-sunrise");
     changeTextColor("#fff");
   } else {
     // use sunrise/sunset image
@@ -251,22 +226,16 @@ function defaultData() {
 }
 
 function getLatLongCoord(response) {
-  //console.log(response.data.coord.lat);
-  //console.log(response.data.coord.lon);
   return response.data.coord.lat;
 }
 
 function displayNext5DaysWeather(response) {
-  //displayNext5Days(now);
-
   let day1 = response.data.daily[1];
   let day2 = response.data.daily[2];
   let day3 = response.data.daily[3];
   let day4 = response.data.daily[4];
   let day5 = response.data.daily[5];
 
-  // test(day1.dt);
-  // convertEpoch(day1.dt);
   displayDay(1, day1.dt);
   displayDay(2, day2.dt);
   displayDay(3, day3.dt);
@@ -319,18 +288,6 @@ function displayNext5DaysWeather(response) {
   displayIcon("img4", day4IconId);
   displayIcon("img5", day5IconId);
 
-  //let day1IconElement = document.getElementById("img1");
-  //let day2IconElement = document.getElementById("img2");
-  //let day3IconElement = document.getElementById("img3");
-  //let day4IconElement = document.getElementById("img4");
-  //let day5IconElement = document.getElementById("img5");
-
-  //day1IconElement.src = `weather_icons/${day1IconID}.png`;
-  //day2IconElement.src = `weather_icons/${day2IconID}.png`;
-  //day3IconElement.src = `weather_icons/${day3IconID}.png`;
-  //day4IconElement.src = `weather_icons/${day4IconID}.png`;
-  //day5IconElement.src = `weather_icons/${day5IconID}.png`;
-
   let day1FeelsLikeElement = document.getElementById("feels-like1");
   let day2FeelsLikeElement = document.getElementById("feels-like2");
   let day3FeelsLikeElement = document.getElementById("feels-like3");
@@ -356,93 +313,12 @@ function displayNext5DaysWeather(response) {
   day5PoPElement.innerHTML = Math.round(day5PoP * 100);
 }
 
-/*
-function displayNext5Days(now) {
-  let weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-
-  const oneDay = 24 * 60 * 60 * 1000;
-  let day1 = new Date(now.getTime() + oneDay);
-  let day2 = new Date(now.getTime() + 2 * oneDay);
-  let day3 = new Date(now.getTime() + 3 * oneDay);
-  let day4 = new Date(now.getTime() + 4 * oneDay);
-  let day5 = new Date(now.getTime() + 5 * oneDay);
-
-  let weekday1Element = document.getElementById("weekday1");
-  let weekday2Element = document.getElementById("weekday2");
-  let weekday3Element = document.getElementById("weekday3");
-  let weekday4Element = document.getElementById("weekday4");
-  let weekday5Element = document.getElementById("weekday5");
-
-  weekday1Element.innerHTML = weekdays[day1.getDay()];
-  weekday2Element.innerHTML = weekdays[day2.getDay()];
-  weekday3Element.innerHTML = weekdays[day3.getDay()];
-  weekday4Element.innerHTML = weekdays[day4.getDay()];
-  weekday5Element.innerHTML = weekdays[day5.getDay()];
-
-  let date1Element = document.getElementById("date1");
-  let date2Element = document.getElementById("date2");
-  let date3Element = document.getElementById("date3");
-  let date4Element = document.getElementById("date4");
-  let date5Element = document.getElementById("date5");
-
-  let month1 = day1.getMonth() + 1;
-  let month2 = day2.getMonth() + 1;
-  let month3 = day3.getMonth() + 1;
-  let month4 = day4.getMonth() + 1;
-  let month5 = day5.getMonth() + 1;
-
-  if (month1 < 10) {
-    month1 = "0" + month1;
-  }
-  if (month2 < 10) {
-    month2 = "0" + month2;
-  }
-  if (month3 < 10) {
-    month3 = "0" + month3;
-  }
-  if (month4 < 10) {
-    month4 = "0" + month4;
-  }
-  if (month5 < 10) {
-    month5 = "0" + month5;
-  }
-
-  let date1 = day1.getDate().toString();
-  let date2 = day2.getDate().toString();
-  let date3 = day3.getDate().toString();
-  let date4 = day4.getDate().toString();
-  let date5 = day5.getDate().toString();
-
-  if (date1 < 10) {
-    date1 = "0" + date1;
-  }
-  if (date2 < 10) {
-    date2 = "0" + date2;
-  }
-  if (date3 < 10) {
-    date3 = "0" + date3;
-  }
-  if (date4 < 10) {
-    date4 = "0" + date4;
-  }
-  if (date5 < 10) {
-    date5 = "0" + date5;
-  }
-
-  date1Element.innerHTML = month1 + "/" + date1;
-  date2Element.innerHTML = month2 + "/" + date2;
-  date3Element.innerHTML = month3 + "/" + date3;
-  date4Element.innerHTML = month4 + "/" + date4;
-  date5Element.innerHTML = month5 + "/" + date5;
-}*/
-
 function useCurrentLocation() {
   navigator.geolocation.getCurrentPosition(displayCurrentLocation);
 }
 
 function catchFunction(e) {
   console.error("myError", e);
-  //console.log(e.message);
 
   if (e.message == "Request failed with status code 404") {
     alert(
@@ -451,8 +327,6 @@ function catchFunction(e) {
   } else {
     alert("An error occurred: " + e.message);
   }
-
-  // The city name "userInput" does not exist. Please check for typos or enter another city name.
 }
 
 function blah(position) {
@@ -496,66 +370,22 @@ function search(event) {
   let userInput = document.querySelector("#search-city");
   let input = userInput.value.toLowerCase();
 
-  // check that input isn't null?
-
   if (input !== "") {
     let city = input;
     blah2(city);
   } else {
     alert(`You must enter a city name before clicking "Search".`);
-    // The city name "userInput" does not exist. Please check for typos or enter another city name.
   }
 }
 
 let searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click", search);
 
-// should i initialize the object inside the function? or outside
-/*
-function getAltText(iconId) {
-  let altTextObject = {
-    "01d": "Yellow Sun",
-    "01n": "Purple Crescent Moon surrounded by Yellow Stars",
-    "02d": "Yellow Sun Behind White Cloud",
-    "02n": "Purple Crescent Moon Behind White Cloud",
-    "03d": "White Cloud",
-    "03n": "White Cloud surrounded by Fuchsia Stars",
-    "04d": "",
-    "04n": "",
-    "09d": "White Cloud with 3 Raindrops",
-    "09n": "White Cloud with 3 Raindrops",
-    "10d": "Yellow Sun Behind White Cloud with 3 Raindrops",
-    "10n": "Purple Crescent Moon Behind White Cloud with 3 Raindrops",
-    "11d": "Yellow Sun Behind White Cloud with Lightning Bolt",
-    "11n":
-      "Purple Crescent Moon Behind White Cloud with Lightning Bolt surrounded by Yellow Stars",
-    "13d": "White Cloud with Snowflakes",
-    "13n": "Purple Crescent Moon Behind White Cloud with Snowflakes",
-    "50d": "Yellow Sun with White Squiggly Mist Lines",
-    "50n": "Purple Crescent Moon with White Squiggly Mist Lines",
-  };
-
-  return altTextObject[iconId];
-}*/
-
-// TO DO: change function to set both image and its alt text, instead of just alt text?
-/*function setAltText(imgId, iconId) {
-  let imgElement = document.getElementById(imgId);
-  imgElement.alt = altTextObject[iconId];
-}*/
-
 function displayIcon(imgId, iconId) {
   let imgElement = document.getElementById(imgId);
   imgElement.src = `weather_icons/${iconId}.png`;
   imgElement.alt = altTextObject[iconId];
 }
-
-/*
-function convertEpoch(dt) {
-  date = new Date(dt * 1000);
-  console.log([date.getDay(), date.getMonth(), date.getDate()]);
-  return [date.getDay(), date.getMonth(), date.getDate()];
-}*/
 
 function displayDay(index, dt) {
   let weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
@@ -577,80 +407,4 @@ function displayDay(index, dt) {
 
   let dateElement = document.getElementById(`date${index}`);
   dateElement.innerHTML = month + "/" + date;
-
-  /*
-  const oneDay = 24 * 60 * 60 * 1000;
-  let day1 = new Date(now.getTime() + oneDay);
-  let day2 = new Date(now.getTime() + 2 * oneDay);
-  let day3 = new Date(now.getTime() + 3 * oneDay);
-  let day4 = new Date(now.getTime() + 4 * oneDay);
-  let day5 = new Date(now.getTime() + 5 * oneDay);
-
-  let weekday1Element = document.getElementById("weekday1");
-  let weekday2Element = document.getElementById("weekday2");
-  let weekday3Element = document.getElementById("weekday3");
-  let weekday4Element = document.getElementById("weekday4");
-  let weekday5Element = document.getElementById("weekday5");
-
-  weekday1Element.innerHTML = weekdays[day1.getDay()];
-  weekday2Element.innerHTML = weekdays[day2.getDay()];
-  weekday3Element.innerHTML = weekdays[day3.getDay()];
-  weekday4Element.innerHTML = weekdays[day4.getDay()];
-  weekday5Element.innerHTML = weekdays[day5.getDay()];
-
-  let date1Element = document.getElementById("date1");
-  let date2Element = document.getElementById("date2");
-  let date3Element = document.getElementById("date3");
-  let date4Element = document.getElementById("date4");
-  let date5Element = document.getElementById("date5");
-
-  let month1 = day1.getMonth() + 1;
-  let month2 = day2.getMonth() + 1;
-  let month3 = day3.getMonth() + 1;
-  let month4 = day4.getMonth() + 1;
-  let month5 = day5.getMonth() + 1;
-
-  if (month1 < 10) {
-    month1 = "0" + month1;
-  }
-  if (month2 < 10) {
-    month2 = "0" + month2;
-  }
-  if (month3 < 10) {
-    month3 = "0" + month3;
-  }
-  if (month4 < 10) {
-    month4 = "0" + month4;
-  }
-  if (month5 < 10) {
-    month5 = "0" + month5;
-  }
-
-  let date1 = day1.getDate().toString();
-  let date2 = day2.getDate().toString();
-  let date3 = day3.getDate().toString();
-  let date4 = day4.getDate().toString();
-  let date5 = day5.getDate().toString();
-
-  if (date1 < 10) {
-    date1 = "0" + date1;
-  }
-  if (date2 < 10) {
-    date2 = "0" + date2;
-  }
-  if (date3 < 10) {
-    date3 = "0" + date3;
-  }
-  if (date4 < 10) {
-    date4 = "0" + date4;
-  }
-  if (date5 < 10) {
-    date5 = "0" + date5;
-  }
-
-  date1Element.innerHTML = month1 + "/" + date1;
-  date2Element.innerHTML = month2 + "/" + date2;
-  date3Element.innerHTML = month3 + "/" + date3;
-  date4Element.innerHTML = month4 + "/" + date4;
-  date5Element.innerHTML = month5 + "/" + date5;*/
 }
